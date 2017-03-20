784b7a43f7aba87a08162273eaab99b51d0f6bad
ChandanB <chandanbrown@yahoo.com>
2017-03-20 10:55:05 -0700

    Setup Folders

---
+++ whale-ios-Chandan/whale-ios-Chandan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  whale-ios-Chandan
+//
+//  Created by Chandan Brown on 3/20/17.
+//  Copyright © 2017 Chandan B. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
